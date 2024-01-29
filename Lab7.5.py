48322c31c6dd5403c181ee98cf836fa5bf41af8e
Panmir <mx872546@gmail.com>
2024-01-29 16:49:27 +0700

    edit

--- Lab7.5.py
+++ Lab7.5.py
@@ -15,20 +15,21 @@ GPIO.add_event_detect(SW1, GPIO.FALLING)
 GPIO.add_event_detect(SW2, GPIO.FALLING)
 display = drivers.Lcd()
 display.lcd_clear()
+display.lcd_display_string(mt+"LAB 7", 1)
 try:
     while True:
         if GPIO.event_detected(SW1):
-            if index+1 <= 12:
+            if index+1 <= 11:
+                index=index+1
+                mt +=" "
                 display.lcd_clear()
                 display.lcd_display_string(mt+"LAB 7", 1)
                 display.lcd_display_string(str(index),2)
-                mt +=" "
-                index=index+1
             sleep(0.5)
         elif GPIO.event_detected(SW2):
             if index-1 >= 0:
-                mt = mt[1:]
                 index=index-1
+                mt = mt[1:]
                 display.lcd_clear()
                 display.lcd_display_string(mt+"LAB 7", 1)
                 display.lcd_display_string(str(index),2)
